3221e36ca3b6c817b5ade66c851b33f11d3c774b
Verzach3 <gabrielcorreacardenas@gmail.com>
2023-05-18 23:30:54 -0500

    add: basic express code & dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+import Express from "express";
+
+// TODO: Add the .env file
+
+const app = Express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+})
+
+
+// TODO: Add the port number to the .env file
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+})
